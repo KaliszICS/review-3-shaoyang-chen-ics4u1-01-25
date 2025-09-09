0fe70ecc00eafa085f774e58551192753075def0
Shaoyang Chen <shaoyang.chen@student.tdsb.on.ca>
2025-09-09 17:31:41 +0000

    Submission v1.1 update ln

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -14,7 +14,7 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
-		System.out.println("In: ");
+		System.out.print("In: ");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         char[] ch = str.toCharArray();
@@ -24,7 +24,7 @@ public class PracticeProblem {
 
 	public static void q2() {
 		//Write question 2 code here
-		System.out.println("In: ");
+		System.out.print("In: ");
         Scanner sc = new Scanner(System.in);
         Boolean bol = sc.nextBoolean();
         System.out.println(!bol);
@@ -33,7 +33,7 @@ public class PracticeProblem {
 
 	public static void q3() {
 		//Write question 3 code here
-		System.out.println("In: ");
+		System.out.print("In: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         boolean flag = n - 5 >= 0;
@@ -42,7 +42,7 @@ public class PracticeProblem {
 
 	public static void q4() {
 		//Write question 4 code here
-		System.out.println("In: ");
+		System.out.print("In: ");
         Scanner sc = new Scanner(System.in);
         double d = sc.nextDouble();
         boolean flag1 = d <= 2;
@@ -53,7 +53,7 @@ public class PracticeProblem {
 
 	public static void q5() {
 		//Write question 5 code here
-		System.out.println("In: ");
+		System.out.print("In: ");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         Boolean flag = input.equals("Hello World");
@@ -62,10 +62,10 @@ public class PracticeProblem {
 
 	public static void q6() {
 		//Write question 6 code here
-		System.out.println("In: ");
+		System.out.print("In: ");
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
-        System.out.println("In: ");
+        System.out.print("In: ");
         int n2 = sc.nextInt();
         boolean flag = n1 <= n2;
         System.out.println(flag);
@@ -73,10 +73,10 @@ public class PracticeProblem {
 
 	public static void q7() {
 		//Write question 7 code here
-		System.out.println("In: ");
+		System.out.print("In: ");
         Scanner sc = new Scanner(System.in);
         double d1 = sc.nextDouble();
-        System.out.println("In: ");
+        System.out.print("In: ");
         double d2 = sc.nextDouble();
         boolean flag = d1 >= d2;
         System.out.println(flag);
