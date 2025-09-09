81be6a95e2d6a50130e83063145f2d21257de161
Shaoyang Chen <shaoyang.chen@student.tdsb.on.ca>
2025-09-09 17:35:40 +0000

    submission v1.3

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -76,7 +76,7 @@ public class PracticeProblem {
 		System.out.print("In: ");
         Scanner sc = new Scanner(System.in);
         double d1 = sc.nextDouble();
-        System.out.println("In: ");
+        System.out.print("In: ");
         double d2 = sc.nextDouble();
         boolean flag = d1 >= d2;
         System.out.println(flag);
